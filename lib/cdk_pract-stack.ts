e922ae3592f38ff6a4cce445ae8cfa24884ca29a
parrgit <kneeepain@gmail.com>
2022-09-30 02:14:11 +0900

    crud s3 on cdk

--- lib/cdk_pract-stack.ts
+++ lib/cdk_pract-stack.ts
@@ -1,10 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3 } from 'aws-cdk-lib'
 
 export class CdkPractStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    })
 
     // The code that defines your stack goes here
 
